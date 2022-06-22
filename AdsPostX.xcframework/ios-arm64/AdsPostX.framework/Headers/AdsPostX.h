650823de462801a930745285ad23ca929fcbab28
shivangvyas2021 <96938980+shivangvyas2021@users.noreply.github.com>
2022-06-22 18:23:29 +0530

    adding xcframework.
    adding xcframework.

---
+++ AdsPostX.xcframework/ios-arm64/AdsPostX.framework/Headers/AdsPostX.h
@@ -0,0 +1,17 @@
+//
+//  AdsPostX.h
+//  AdsPostX
+//
+//  Created by shivang vyas on 20/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdsPostX.
+FOUNDATION_EXPORT double AdsPostXVersionNumber;
+
+//! Project version string for AdsPostX.
+FOUNDATION_EXPORT const unsigned char AdsPostXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdsPostX/PublicHeader.h>
+
